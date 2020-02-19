f7a480c393e2dc5afb4d5454f72e29c385fa67e8
vishal <vishalsharma16171718@gmail.com>
2020-02-19 15:26:07 +0530

    fixed view list segment error

--- view_list.c
+++ view_list.c
@@ -7,9 +7,11 @@ void view_list()
     int test = 0;
     system("clear");
     printf("\nACC. NO.\tNAME\t\t\tADDRESS\t\t\tPHONE\n");
-    while(fscanf(view, "%s %s %d/%d/%d %d %s %s %s %s %.2lf %d/%d/%d\n", add.acc_no, add.name, add.dob.day, add.dob.month, add.dob.year, add.age, add.address, add.citizenship, add.phone, add.acc_type, add.amt, add.deposit.day, add.deposit.month, add.deposit.year)== 14)
+    // while(fscanf(view, "%s %s %d/%d/%d %d %s %s %s %s %.2lf %d/%d/%d\n",add.acc_no, add.name, add.dob.day, add.dob.month, add.dob.year, add.age, add.address, add.citizenship, add.phone, add.acc_type, add.amt, add.deposit.day, add.deposit.month, add.deposit.year)== 14)
+    while(fscanf(view, "%s %s %d/%d/%d %d %s %s %s %s %f %d/%d/%d\n", add.acc_no, add.name, &add.dob.day, &add.dob.month, &add.dob.year, &add.age, add.address, add.citizenship, add.phone, add.acc_type, &add.amt, &add.deposit.day, &add.deposit.month, &add.deposit.year) != EOF)
     {
-        fprintf(stdout, "%s\t%s\t\t\t%s\t\t\t%s\n", add.acc_no, add.name, add.address, add.phone);
+        
+        printf("%s\t|%s\t\t\t|%s\t\t\t|%s\n", add.acc_no, add.name, add.address, add.phone);
         test++;
     }
 
